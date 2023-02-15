3e5a69f40edcd0e1be1c58e9a0fdf4ed66ce0bdc
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-02-15 14:11:08 -0800

    Declarative Approach

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { catchError, EMPTY, Observable } from 'rxjs';
+import { catchError, EMPTY } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
@@ -11,21 +10,16 @@ import { ProductService } from './product.service';
     styleUrls: ['./product-list.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
     pageTitle = 'Product List';
     errorMessage = '';
     categories: ProductCategory[] = [];
 
-    products$: Observable<Product[]> | undefined;
+    products$ = this.productService.products$.pipe(
+        catchError(err => { this.errorMessage = err; return EMPTY; })
+    );
 
     constructor(private productService: ProductService) { }
-
-    ngOnInit(): void {
-        this.products$ = this.productService.getProducts()
-            .pipe(
-                catchError(err => { this.errorMessage = err; return EMPTY; })
-            );
-    }
 
     onAdd(): void {
         console.log('Not yet implemented');
