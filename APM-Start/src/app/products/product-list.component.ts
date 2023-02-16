962f224c9c47eef959d3a69eeb7615d732e2f986
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-02-16 13:40:37 -0800

    productSimpleFilter

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { catchError, EMPTY } from 'rxjs';
+import { catchError, EMPTY, filter, map } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
 import { ProductService } from './product.service';
@@ -14,10 +14,19 @@ export class ProductListComponent {
     pageTitle = 'Product List';
     errorMessage = '';
     categories: ProductCategory[] = [];
+    selectedCategoryId = 1;
 
     products$ = this.productService.productsWithCategory$.pipe(
         catchError(err => { this.errorMessage = err; return EMPTY; })
     );
+
+    productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+        map(products => products.filter(product =>
+            this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+        )),
+        catchError(err => { this.errorMessage = err; return EMPTY; })
+    );
+
 
     constructor(private productService: ProductService) { }
 
