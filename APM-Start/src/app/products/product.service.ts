015100844950e7ffbcec374d572b11bf489a4069
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-03-06 16:30:07 -0800

    Higher order observables: map, concatMap

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject, catchError, combineLatest, map, merge, Observable, sca
 
 import { Product } from './product';
 import { ProductCategoryService } from "../product-categories/product-category.service";
+import { SupplierService } from '../suppliers/supplier.service';
 
 @Injectable({
     providedIn: 'root'
@@ -94,7 +95,7 @@ export class ProductService {
         shareReplay(1)
     );
 
-    constructor(private http: HttpClient, private productCategoryService: ProductCategoryService) {}
+    constructor(private http: HttpClient, private productCategoryService: ProductCategoryService, private supplierService: SupplierService) {}
 
     private fakeProduct(): Product {
         return {
