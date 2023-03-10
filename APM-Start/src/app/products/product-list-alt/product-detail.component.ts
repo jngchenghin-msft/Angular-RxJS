56cc64efeb01f0107fa8e2d1f42573a9ee636be6
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-03-09 16:08:38 -0800

    Anciliary Streams

--- APM-Start/src/app/products/product-list-alt/product-detail.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,32 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { catchError, EMPTY, Subject } from 'rxjs';
-import { Supplier } from 'src/app/suppliers/supplier';
+import { catchError, EMPTY, map, Subject } from 'rxjs';
 
 import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'pm-product-detail',
-  templateUrl: './product-detail.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'pm-product-detail',
+    templateUrl: './product-detail.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
-  private errorMessageSubject = new Subject<string>();
-  errorMessageSubject$ = this.errorMessageSubject.asObservable();
+    private errorMessageSubject = new Subject<string>();
+    errorMessageSubject$ = this.errorMessageSubject.asObservable();
 
-  constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { }
 
-  productDetail$ = this.productService.selectedProduct$.pipe(
-    catchError(err => {
-      this.errorMessageSubject.next(err);
-      return EMPTY;
-    }
-  ));
+    productDetail$ = this.productService.selectedProduct$.pipe(
+        catchError(err => {
+            this.errorMessageSubject.next(err);
+            return EMPTY;
+        }
+        ));
 
-  productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
-    catchError(err => {
-      this.errorMessageSubject.next(err);
-      return EMPTY;
-    }
-  ));
+    productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
+        catchError(err => {
+            this.errorMessageSubject.next(err);
+            return EMPTY;
+        }
+        ));
+
+    pageTitle$ = this.productDetail$.pipe(
+        map(product => product ? `Product Detail for: ${product.productName}` : null)
+    );
 }
